47b4c7ca050c36032fe94bf591dded83804335d3
Vjeko <vjeko@vjeko.com>
2019-11-13 17:40:20 +0100

    Initial - 04-gulp-useful

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,38 @@
-function helloWorld(done) {
-    console.log("Hello, World!");
-    done();
-}
+const gulp = require("gulp");
+const del = require("del");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify-es").default;
+const cleanCss = require("gulp-clean-css");
 
-exports.hello = helloWorld;
+const config = require("./gulp.config.json");
+
+const path = {
+    js: `${config.root}/${config.path.js}`,
+    css: `${config.root}/${config.path.css}`
+};
+const bundleJsFileName = `${config.package}.min.js`;
+const bundleCssFileName = `${config.package}.min.css`;
+const pathBundleJs = `${path.js}/${bundleJsFileName}`;
+const pathBundleCss = `${path.css}/${bundleCssFileName}`;
+const globJs = `${path.js}/*.js`;
+const globCss = `${path.css}/*.css`;
+
+const deleteBundleJs = () => del(pathBundleJs);
+const deleteBundleCss = () => del(pathBundleCss);
+
+const bundleJs = () => gulp
+    .src([globJs, `!${path.js}/start.js`])
+    .pipe(concat(bundleJsFileName))
+    .pipe(uglify())
+    .pipe(gulp.dest(path.js))
+
+const bundleCss = () => gulp
+    .src(globCss)
+    .pipe(concat(bundleCssFileName))
+    .pipe(cleanCss())
+    .pipe(gulp.dest(path.css))
+
+const buildJs = gulp.series(deleteBundleJs, bundleJs);
+const buildCss = gulp.series(deleteBundleCss, bundleCss);
+
+exports.build = gulp.parallel(buildJs, buildCss);
--- src/add-in/Simpler.ControlAddIn.al
+++ src/add-in/Simpler.ControlAddIn.al
@@ -1,19 +1,8 @@
 controladdin Simpler
 {
-    Scripts =
-        'src/add-in/scripts/numeral.min.js',
-        'src/add-in/scripts/simpler.constants.js',
-        'src/add-in/scripts/simpler.interface.js',
-        'src/add-in/scripts/simpler.state.js',
-        'src/add-in/scripts/simpler.ui.js';
-
+    Scripts = 'src/add-in/scripts/simpler.min.js';
     StartupScript = 'src/add-in/scripts/start.js';
-
-    StyleSheets =
-        'src/add-in/styles/simpler.core.css',
-        'src/add-in/styles/simpler.data.css',
-        'src/add-in/styles/simpler.entry.css',
-        'src/add-in/styles/simpler.summary.css';
+    StyleSheets = 'src/add-in/styles/simpler.min.css';
 
     RequestedHeight = 400;
     RequestedWidth = 600;
